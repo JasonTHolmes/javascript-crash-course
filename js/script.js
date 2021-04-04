99e9e69f47c0f187ec4693629c106f9e7f0c7bce
Jason Holmes <JasonThomasHolmes@Gmail.com>
2021-04-04 00:11:54 -0700

    Started the course over for continuity.

--- js/script.js
+++ js/script.js
@@ -1,179 +1,95 @@
-/*  console.log('Hello World!');
-alert('yo!');
+console.log('hello');
 
-var b = "smoothie"
+// alert('hello this is Jason');
 
-console.log(b)
+// How to write a comment inline
 
-var age = prompt('what is your age?');
+// Variables
+var b = 'smoothie';
+console.log(b);
 
-document.getElementById('text').innerText = age;
+var someNumber = 45;
+// console.log(someNumber);
 
+// Manipulate DOM with JavaScript
+/* ... It's just a fancy way of saying
+change HTML with JavaScript */
+
+// var age = prompt('What is your age?');
+
+// document.getElementById('someText').innerHTML = age;
+
+// Numbers in Javascript
 var num1 = 10;
+
+// increment num1 by 1
 num1++;
 
+// decrement num1 by 1
 num1--;
+console.log(num1);
 
+// divide, multiply *, remainder %
+console.log(num1 % 6);
+
+// increment/decrement by any number you want
 num1 += 10;
 console.log(num1);
 
+/* Functions
+1 create a function
+2. Call the function
+*/
+
+// Create
 function fun(){
-     alert('this is a function')
+console.log('This is a function');
 }
+
+// Call
 fun();
 
-// creates a function that takes a name and returns "hello" followed by name
+/* Let's create a function that takes in a name
+and says hello followed by your name
 
-// for example
+For example:
 
-// if name is "Jay" - return "Hello Jay"
+Name: "Jason"
+Return: "Hello Jason"
+*/
+
 
 function greeting(yourName) {
-    var result = 'hello ' + yourName;
-    console.log(result)
+    var result = 'Hello ' + yourName; // String Concatenation
+    console.log(result);
 }
 
-var name = prompt('What is your name?')
-greeting(name)
+// var name = prompt('What is your name?');
+// greeting(name);
 
-function sumNumbers(a, b){
-    console.log(a + b)
+// How do arguments work in functions?
+// How do we add 2 numbers together  in a function
+
+function sumNumbers (num1, num2) {
+    var result = num1 + num2;
+    console.log(num1 + num2);
 }
 
-sumNumbers(4, 3) 
+sumNumbers(10, 10);
 
-//while loops
+/* While loops
 
 var num = 0;
 
-while (num < 100) {
+while (num < 100){
     num += 1;
     console.log(num);
 }
 
-//for loops
+*/
 
-for (let num = 0; num < 420; num++) {
+// For loop
+for (let num = 0; num <= 100; num++) {
     console.log(num);
 }
-// Data Types
-// numbers, strings, objects, boolean, array, undefined, null
 
-//strings
-
-let fruit = 'banana';
-let moreFruit = 'banana\napple'
-
-console.log(moreFruit);
-console.log(fruit.length);
-console.log(fruit.indexOf('a'));
-console.log(fruit.slice(2, 6));
-console.log(fruit.replace('ban', '123'));
-console.log(fruit.toUpperCase());
-console.log(fruit.toLowerCase());
-console.log(fruit.charAt(2));
-console.log(fruit[3]);
-console.log(fruit.split(''));
-
-//Arrays in Javascript
-*/
-let fruit = ['orange', 'banana','pineapples', 'apple'];
-
-fruit = new Array ('banana', 'orange', 'apple', 'pineapples');
-
-//console.log(fruit[2]); //access value at index 2
-
-//fruit[0] = 'pear';
-//console.log(fruit);
-
-/*for (let i = 0; i < fruit.length; i++){
-    console.log(fruit[i])
-}*/
-
-
-// array common methods
-console.log('to string', fruit.toString());
-console.log(fruit.join( ' * '));
-console.log(fruit, fruit.pop(), fruit); //removes last item
-console.log(fruit.push('blackberries'), fruit); // appends
-console.log(fruit[3]);
-fruit[fruit.length] = 'new fruit'; // same as push
-console.log(fruit);
-fruit.shift(); // removes first element from an array
-console.log(fruit);
-fruit.unshift('kiwi'); // add first element to an array
-console.log(fruit);
-let vegetables = ['asparagus', 'tomato', 'broccoli'];
-let allGroceries = fruit.concat(vegetables);
-console.log(allGroceries);
-console.log(allGroceries.slice(1, 4));
-console.log(allGroceries.reverse());
-console.log(allGroceries.sort())
-
-let someNumbers = [5, 10, 2, 25, 3, 255, 1, 2, 5, 334, 321, 2];
-console.log(someNumbers.sort(function(a, b) {return a-b;})); // sorted in ascending order
-console.log(someNumbers.sort(function(a, b) {return b-a;})); // sorted in descending order
-
-let emptyArray = new Array();
-for (let num = 0; num <= 10; num++) {
-    emptyArray.push(num)
-}
-console.log(emptyArray)
-
-//Objects in Javascript
-
-let student = {
-    first: 'Jason',
-    last: 'Holmes',
-    age:37,
-    height:182,
-    studentInfo: function(){
-        return this.first +  '\n' + this.last + '\n' + this.age;
-    }
-};
-
-// console.log(student.first);
-// console.log(student['first']);
-// student.first = "notJason"; // Change the value
-// console.log(student['first']);
-// student['first'] = 'newjason'; //another way to change the value. in the video, Qasi is not putting the quotations around the keys.
-// console.log(student['first']);
-student.age++;
-console.log(student.age);
-console.log(student.studentInfo());
-
-// Conditionals, Control Flows (if else)
-// 18-35 is my target demographic
-// && AND bolean statement
-// || OR
-var age = 45;
-
-if((age >= 18) && (age <= 35) ){
-    status = 'target demo';
-    console.log(status)
-}else{
-    status = 'Not my audience';
-    console.log(status)
-}
-
-// Switch Statements
-//Differnetiate betweena  weekdaya nd a weekend
-// 0 Sunday
-// 6 is Saturday
-// 4 Thursday
-
-switch (6) {
-    case 0:
-        text = 'weekend';
-        break;
-    case 5:
-        text = "weekend";
-        break;
-    case 6:
-        text = "weekend"
-        break;
-    default:
-        text = "weekday";
-}
-
-console.log(text)
